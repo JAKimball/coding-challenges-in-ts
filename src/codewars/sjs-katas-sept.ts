d9dfba0f3e3aa7c3ec72844cb6572373f06b10ef
Jonathan A. Kimball <14268609+JAKimball@users.noreply.github.com>
2023-09-24 17:34:55 -0700

    feat: updated test code for duplicateEncode() to quickly test 1000 random strings and added a new version of the function that work with just a single pass. (TODO: benchmark to see which is faster)

--- src/codewars/sjs-katas-sept.ts
+++ src/codewars/sjs-katas-sept.ts
@@ -1,5 +1,7 @@
 // Seattle JS - Code Katas - September 2023
 
+import { performance } from 'perf_hooks'
+
 export function capitalize(s: string) {
   let e = ''
   let o = ''
@@ -18,6 +20,9 @@ capitalize('abcdef') // ?
 if (import.meta.vitest) {
   const { assert, describe, expect, it } = import.meta.vitest
   describe('capitalize() basic tests', function () {
+    it('Testing for empty string', () => {
+      assert.deepEqual(capitalize(''), ['', ''])
+    })
     it('Testing for abcdef', () => {
       assert.deepEqual(capitalize('abcdef'), ['AbCdEf', 'aBcDeF'])
     })
@@ -75,7 +80,7 @@ if (import.meta.vitest) {
 
 //=============
 
-export function duplicateEncode(word: string) {
+export function duplicateEncode1(word: string) {
   const charSet = new Set<string>()
   const multiSet = new Set<string>()
   const upWord = word.toUpperCase()
@@ -87,6 +92,34 @@ export function duplicateEncode(word: string) {
   return result
 }
 
+export function duplicateEncode(word: string) {
+  const charIndex = new Map<string, number>()
+  const upWord = word.toUpperCase()
+  const result = []
+
+  for (let i = 0; i < word.length; i++) {
+    const c = upWord[i]
+    const index = charIndex.get(c)
+    if (index !== undefined) {
+      result[index] = ')'
+      result[i] = ')'
+    } else {
+      charIndex.set(c, i)
+      result[i] = '('
+    }
+  }
+
+  return result.join('')
+}
+
+const word = 'abcdefghijklmnopqrstuvwxyz'.repeat(10000)
+
+const t0 = performance.now()
+const result = duplicateEncode(word)
+const t1 = performance.now()
+
+console.log(`Execution time: ${t1 - t0} ms`)
+
 if (import.meta.vitest) {
   const { assert, describe, expect, it } = import.meta.vitest
   describe('example', function () {
@@ -95,6 +128,30 @@ if (import.meta.vitest) {
       assert.strictEqual(duplicateEncode('recede'), '()()()')
       assert.strictEqual(duplicateEncode('Success'), ')())())', 'should ignore case')
       assert.strictEqual(duplicateEncode('(( @'), '))((')
+    })
+  })
+
+  describe('Random tests', () => {
+    const randomTests = Array.from({ length: 1000 }, () => {
+      const length = Math.floor(Math.random() * 100) + 1
+      const word = Array.from({ length }, () => {
+        const charCode = Math.floor(Math.random() * 26) + 97
+        return Math.random() < 0.5
+          ? String.fromCharCode(charCode)
+          : String.fromCharCode(charCode).toUpperCase()
+      }).join('')
+      const upWord = word.toUpperCase()
+      const expected = Array.from({ length }, (_, i) => {
+        const char = upWord[i]
+        return upWord.indexOf(char) === upWord.lastIndexOf(char) ? '(' : ')'
+      }).join('')
+      return [word, expected]
+    })
+
+    it('Testing 1000 random strings', () => {
+      const results = randomTests.map(([word]) => duplicateEncode(word))
+      const expected = randomTests.map(([, expected]) => expected)
+      assert.deepEqual(results, expected)
     })
   })
 }
@@ -123,6 +180,17 @@ export function rgb3(r: number, g: number, b: number): string {
   return (part(r) + part(g) + part(b)).toUpperCase()
   // return [part(r), part(g), part(b)].join('').toUpperCase()
 }
+
+/*
+  TODO: Look into why we get a V8 heap out of memory error when benchmarking
+  with `(part(r) + part(g) + part(b)).toUpperCase()` and not with
+  `[part(r), part(g), part(b)].join('').toUpperCase()` which is slower.
+  Heap out of memory error occurs with after several million calls but there
+  does seem to be a memory leak. But I need to find out if this is specific to
+  vitest or if it is a V8 issue. Does benchmarking with jest or mocha have the
+  same issue? Does running the code in node have the same issue? Does benchmarking
+  suppress garbage collection? Many questions...
+*/
 
 rgb3(-1, 0xff, 0x100) // ?
 
