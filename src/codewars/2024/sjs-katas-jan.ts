2066660db1d97be1af7e839f602d582df51e7e67
Jonathan A. Kimball <14268609+JAKimball@users.noreply.github.com>
2024-01-26 08:59:42 -0800

    feat: fix test for jan 2024 graph exercise

--- src/codewars/2024/sjs-katas-jan.ts
+++ src/codewars/2024/sjs-katas-jan.ts
@@ -23,11 +23,11 @@ export function tribonacci([a, b, c]: [number, number, number], n: number): numb
 
 // https://www.codewars.com/kata/53897d3187c26d42ac00040d/train/javascript
 
-class TNode {
-  edges: TNode[]
+class Node {
+  edges: Node[]
   value: unknown
 
-  constructor(value: unknown, edges = []) {
+  constructor(value: unknown, edges: Node[] = []) {
     this.value = value
     this.edges = edges
   }
@@ -39,162 +39,109 @@ function getRoute(a: Node, b: Node) {
   return false
 }
 
-describe('tests suite', function () {
-  const Node = New(TNode)
+// prettier-ignore
+if (import.meta.vitest) {
+  const { assert, describe, expect, it } = import.meta.vitest
 
-  const { strictEqual } = require('chai').assert
+  describe('tests suite', function () {
+    const { strictEqual } = assert
 
-  function doTest(a, b, expected, graphString) {
-    const log = `for getRoute(${a.value}, ${b.value}) in graph:\n${graphString}\n`
-    const actual = getRoute(a, b)
-    strictEqual(actual, expected, log)
-  }
-
-  it('simple acyclic graph', function () {
-    const ascii =
-      'A ----> B ----> D' +
-      '\n' +
-      '↑       |' +
-      '\n' +
-      '|       |     E' +
-      '\n' +
-      '|       ↓' +
-      '\n' +
-      '|-----> C' +
-      '\n'
-    const E = new Node('E'),
-      D = new Node('D'),
-      C = new Node('C')
-    const B = new Node('B', [C, D])
-    const A = new Node('A', [B, C])
-
-    const edges = { A, B, C, D, E }
-    const matrix = {
-      A: {
-        A: false,
-        B: true,
-        C: true,
-        D: true,
-        E: false,
-      },
-      B: {
-        A: false,
-        B: false,
-        C: true,
-        D: true,
-        E: false,
-      },
-      C: {
-        A: false,
-        B: false,
-        C: false,
-        D: false,
-        E: false,
-      },
-      D: {
-        A: false,
-        B: false,
-        C: false,
-        D: false,
-        E: false,
-      },
-      E: {
-        A: false,
-        B: false,
-        C: false,
-        D: false,
-        E: false,
-      },
-    }
-    for (const a in matrix)
-      for (const b in matrix[a]) doTest(edges[a], edges[b], matrix[a][b], ascii)
-  })
-
-  it('loop ', function () {
-    const ascii = 'node----' + '\n' + '↑      |' + '\n' + '|      |' + '\n' + '|------|' + '\n'
-    const node = new Node('node')
-    node.edges = [node]
-    doTest(node, node, true, ascii)
-  })
-
-  it('simple cyclic graph ', function () {
-    const ascii =
-      'A----->B' +
-      '\n' +
-      '↑      |' +
-      '\n' +
-      '|      |       F' +
-      '\n' +
-      '|      |' +
-      '\n' +
-      '|      ↓' +
-      '\n' +
-      'D<-----C ----> E' +
-      '\n'
-    const E = new Node('E'),
-      F = new Node('F')
-    const A = new Node('A')
-    const D = new Node('D', [A])
-    const C = new Node('C', [D, E])
-    const B = new Node('B', [C])
-    A.edges = [B]
-
-    const edges = { A, B, C, D, E, F }
-    const matrix = {
-      A: {
-        A: true,
-        B: true,
-        C: true,
-        D: true,
-        E: true,
-        F: false,
-      },
-      B: {
-        A: true,
-        B: true,
-        C: true,
-        D: true,
-        E: true,
-        F: false,
-      },
-      C: {
-        A: true,
-        B: true,
-        C: true,
-        D: true,
-        E: true,
-        F: false,
-      },
-      D: {
-        A: true,
-        B: true,
-        C: true,
-        D: true,
-        E: true,
-        F: false,
-      },
-      E: {
-        A: false,
-        B: false,
-        C: false,
-        D: false,
-        E: false,
-        F: false,
-      },
-      F: {
-        A: false,
-        B: false,
-        C: false,
-        D: false,
-        E: false,
-        F: false,
-      },
+    function doTest(a, b, expected, graphString) {
+      const log = `for getRoute(${a.value}, ${b.value}) in graph:\n${graphString}\n`
+      const actual = getRoute(a, b)
+      strictEqual(actual, expected, log)
     }
 
-    for (const a in matrix)
-      for (const b in matrix[a]) doTest(edges[a], edges[b], matrix[a][b], ascii)
+	it('simple acyclic graph', function () {
+		const ascii = 
+			"A ----> B ----> D" + "\n" +
+			"↑       |"         + "\n" +
+			"|       |     E"   + "\n" +
+			"|       ↓"         + "\n" +
+			"|-----> C"         + "\n"
+		;
+		const E = new Node('E'), D = new Node('D'), C = new Node('C');
+		const B = new Node('B', [C, D]);
+		const A = new Node('A', [B, C]);
+
+		const edges = {A, B, C, D, E};
+		const matrix = {
+			'A' : {
+				'A' : false, 'B' : true, 'C' : true, 'D' : true, 'E' : false
+			},
+			'B' : {
+				'A' : false, 'B' : false, 'C' : true, 'D' : true, 'E' : false
+			},
+			'C' : {
+				'A' : false, 'B' : false, 'C' : false, 'D' : false, 'E' : false
+			},
+			'D' : {
+				'A' : false, 'B' : false, 'C' : false, 'D' : false, 'E' : false
+			},
+			'E' : {
+				'A' : false, 'B' : false, 'C' : false, 'D' : false, 'E' : false
+			},
+		};
+		for (const a in matrix)
+			for (const b in matrix[a])
+				doTest(edges[a], edges[b], matrix[a][b], ascii);
+	});
+
+	it('loop ', function () {
+		const ascii =
+			"node----" + "\n" +
+			"↑      |" + "\n" +
+			"|      |" + "\n" +
+			"|------|" + "\n"
+		;
+		const node = new Node('node');
+		node.edges = [node];
+		doTest(node, node, true, ascii);
+	});
+
+	it('simple cyclic graph ', function () {
+		const ascii = 
+			"A----->B"         + "\n" +
+			"↑      |"         + "\n" +
+			"|      |       F" + "\n" +
+			"|      |"         + "\n" +
+			"|      ↓"         + "\n" +
+			"D<-----C ----> E" + "\n"
+		;
+		const E = new Node('E'), F = new Node('F');
+		const A = new Node('A');
+		const D = new Node('D', [A]);
+		const C = new Node('C', [D, E]);
+		const B = new Node('B', [C]);
+		A.edges = [B];
+
+		const edges = {A, B, C, D, E, F};
+		const matrix = {
+			'A' : {
+				'A' : true, 'B' : true, 'C' : true, 'D' : true, 'E' : true, 'F' : false
+			},
+			'B' : {
+				'A' : true, 'B' : true, 'C' : true, 'D' : true, 'E' : true, 'F' : false
+			},
+			'C' : {
+				'A' : true, 'B' : true, 'C' : true, 'D' : true, 'E' : true, 'F' : false
+			},
+			'D' : {
+				'A' : true, 'B' : true, 'C' : true, 'D' : true, 'E' : true, 'F' : false
+			},
+			'E' : {
+				'A' : false, 'B' : false, 'C' : false, 'D' : false, 'E' : false, 'F' : false
+			},
+			'F' : {
+				'A' : false, 'B' : false, 'C' : false, 'D' : false, 'E' : false, 'F' : false
+			},
+		};
+
+		for (const a in matrix)
+			for (const b in matrix[a])
+				doTest(edges[a], edges[b], matrix[a][b], ascii);    })
   })
-})
+}
 
 /***************************
  * Example test template
