752e50a6a661ebc60036a460a0f86edb87374751
Jonathan A. Kimball <14268609+JAKimball@users.noreply.github.com>
2024-05-15 01:33:58 -0700

    feat: add sjs-katas-april.ts from the April coding katas meetup

---
+++ src/codewars/2024/sjs-katas-april.ts
@@ -0,0 +1,45 @@
+import { performance } from 'perf_hooks'
+
+import { memoize } from '../../dp/memoize.js'
+
+function addBinary(a: number, b: number) {
+  return (a + b).toString(2)
+}
+
+export function tribonacci([a, b, c]: [number, number, number], n: number): number[] {
+  const result = [a, b, c]
+
+  if (n <= 3) {
+    result.length = n
+    return result
+  }
+
+  do {
+    ;[a, b, c] = [b, c, a + b + c]
+    result.push(c)
+    n--
+  } while (n > 3)
+
+  return result
+}
+
+/***************************
+ * Example test template
+ */
+
+export function isTriangle(a: number, b: number, c: number): boolean {
+  const sum = a + b + c
+  return sum - 2 * Math.max(a, b, c) > 0
+}
+
+// in-source test suites
+if (import.meta.vitest) {
+  const { assert, describe, expect, it } = import.meta.vitest
+  describe('isTriangle tests', () => {
+    it('should pass basic tests', () => {
+      expect(isTriangle(1, 2, 2)).toBe(true)
+      expect(isTriangle(7, 2, 2)).toBe(false)
+      expect(isTriangle(1, 2, 3)).toBe(false)
+    })
+  })
+}
