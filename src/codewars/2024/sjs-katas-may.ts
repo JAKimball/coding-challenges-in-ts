d6212b58bbed1d4882da272b473f7a29b2b49ba8
Jonathan A. Kimball <14268609+JAKimball@users.noreply.github.com>
2024-07-16 22:05:23 -0700

    feat: add sjs-katas-may.ts

---
+++ src/codewars/2024/sjs-katas-may.ts
@@ -0,0 +1,24 @@
+import { performance } from 'perf_hooks'
+
+import { memoize } from '../../dp/memoize.js'
+
+/***************************
+ * Example test template
+ */
+
+export function isTriangle(a: number, b: number, c: number): boolean {
+  const sum = a + b + c
+  return sum - 2 * Math.max(a, b, c) > 0
+}
+
+// in-source test suites
+if (import.meta.vitest) {
+  const { assert, describe, expect, it } = import.meta.vitest
+  describe('isTriangle tests', () => {
+    it('should pass basic tests', () => {
+      expect(isTriangle(1, 2, 2)).toBe(true)
+      expect(isTriangle(7, 2, 2)).toBe(false)
+      expect(isTriangle(1, 2, 3)).toBe(false)
+    })
+  })
+}
